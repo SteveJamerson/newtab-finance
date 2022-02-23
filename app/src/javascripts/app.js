56f233ee6bf26d557a32db3d5c81efba850e590d
STEVE\steve <stevejamerson@outlook.com>
2022-02-23 10:46:47 -0300

    init; validation; delete

--- app/src/javascripts/app.js
+++ app/src/javascripts/app.js
@@ -5,11 +5,20 @@ import "./price";
 
 import { TransactionController } from "./controllers/transactionController.js";
 
-const form = document.getElementById("form-add");
-const controller = new TransactionController(form);
-console.log(controller);
+document.addEventListener("DOMContentLoaded", function () {
+   "use strict";
+   const form = document.getElementById("form-add");
+   const controller = new TransactionController(form);
 
-form.addEventListener("submit", (e) => {
-   e.preventDefault();
-   controller.add();
+   const clear = document.getElementById("clear");
+
+   form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      controller.add();
+   });
+
+   clear.addEventListener("click", (e) => {
+      e.preventDefault();
+      controller.delete("all");
+   });
 });
--- app/src/javascripts/controllers/transactionController.js
+++ app/src/javascripts/controllers/transactionController.js
@@ -1,21 +1,70 @@
+import { environment } from "../environment.js";
 import { Transaction } from "../models/transaction.js";
 import { Transactions } from "../models/transactions.js";
+import { TransactionService } from "../services/transactionService.js";
 import { TransactionsView } from "../views/transactionsView.js";
 
 export class TransactionController {
    _transactions = new Transactions();
    _transactionsView = new TransactionsView("#invoiceView");
+   _transactionService = new TransactionService();
    _form;
+   id = null;
+   loading = true;
 
    constructor(_form) {
       this._form = _form;
       this._transactionsView.update(this._transactions);
+      this.init();
+   }
 
-      console.log(this._form);
+   init() {
+      this._updateView();
+      this._transactionService
+         .get()
+         .then(({ records }) => {
+            this.id = records[0]?.id;
+            this._transactions.import(
+               JSON.parse(records[0]?.fields.Json || "[]")
+            );
+         })
+         .finally(() => {
+            this.loading = false;
+            this._updateView();
+         });
    }
 
    add() {
       const { type, name, date, quantity, price } = this._form.elements;
+
+      const validate = this._validation([
+         {
+            element: type,
+            regex: /(-|\+)/,
+         },
+         {
+            element: name,
+            regex: /^.{1,}$/,
+         },
+         {
+            element: date,
+            regex: /\d{4}-\d{2}-\d{2}/,
+         },
+         {
+            element: quantity,
+            regex: /^[1-9]|[0]{1,}[1-9]/,
+         },
+         {
+            element: price,
+            regex: /^R\$\s?((\d{1,3}\.)+)?(\d{1,3}\,)(\d{2})$/,
+         },
+      ]);
+
+      if (validate.length) {
+         alert(`Campo(s) inválidos: ${validate.map((i) => i.name).join("; ")}`);
+         return;
+      }
+
       const transaction = Transaction.create(
          type.value,
          name.value,
@@ -23,7 +72,27 @@ export class TransactionController {
          quantity.value,
          price.value
       );
+
       this._transactions.add(transaction);
+      this._updateView();
+
+      const data = {
+         fields: {
+            Responsavel: environment.USER,
+            Json: JSON.stringify(this._transactions.list()),
+         },
+      };
+      this._transactionService[this.id ? "patch" : "post"](data, this.id).then(
+         ({ id }) => (this.id = id)
+      );
+   }
+
+   delete(index) {
+      if (index === "all" && this.id) {
+         this._transactionService.delete(this.id);
+         this.id = null;
+      }
+      this._transactions.delete(index);
       this._updateView();
    }
 
@@ -32,18 +101,16 @@ export class TransactionController {
    }
 
    _updateView() {
-      this._transactionsView.update(this._transactions);
+      this._transactionsView.update(this._transactions, this.loading);
    }
 
-   _validation() {
-      const { type, name, date, quantity, price } = this._form.elements;
-      [type, name, date, quantity, price].forEach((v) => {
-         v.value === ""
-            ? () => {
-                 console.log(v.value);
-                 throw Error(`Campo ${v.name} é obrigatório.`);
-              }
-            : console.log(v.value);
-      });
+   _validation(arr) {
+      return arr
+         .map(({ element, regex }) => {
+            const is = new RegExp(regex).test(element.value);
+            element.classList[is ? "remove" : "add"]("invalid");
+            return { valid: is, name: element.name };
+         })
+         .filter((v) => !v.valid);
    }
 }
